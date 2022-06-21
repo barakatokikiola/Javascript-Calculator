8ed82c735fc5bbceef7c80ff254e4343c8180e49
barakatokikiola <barakattajudeen2018@gmail.com>
2022-06-21 22:15:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let username = prompt('Enter your name');
+let height = prompt('Enter your height in cm');
+let country = prompt('Enter your country');
+
+alert(`My name is ${username}\nI am ${height}cm tall\nI'm from ${country}`);
