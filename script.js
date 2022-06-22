1e4c863476bb0da59cd5daebe0ecc28be805ca61
barakatokikiola <barakattajudeen2018@gmail.com>
2022-06-22 23:00:30 +0100

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let username = prompt('Enter your name');
-let height = prompt('Enter your height in cm');
-let country = prompt('Enter your country');
+let val1 = parseFloat(prompt('Enter 1st Value'));
+let operator = prompt('Specify the operation to be performed +, -, *, /');
+let val2 = parseFloat(prompt('Enter 2nd Value'));
+let result;
+ if(isNaN(val1) || isNaN(val2)) {
+    alert('Please refresh the page and enter a valid number')
+ } else {
+    if (operator === '+'){
+        result = val1 + val2;
+    } else if (operator === '-'){
+        result = val1 - val2;
+    } else if (operator === '*'){
+        result = val1 * val2;
+    } else if (operator === '/'){
+        result = val1 / val2;
+    }
+ }
+ 
 
-alert(`My name is ${username}\nI am ${height}cm tall\nI'm from ${country}`);
+alert(`${val1} ${operator} ${val2} = ${result}`);
